30c04f5eeb9fa45e9cc7f4d5544dee60566efe8e
aizaqazman <95851187+aizaqazman@users.noreply.github.com>
2025-04-24 16:48:26 +0800

    first commit

---
+++ app/dashboard/survey-dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Table from '@/components/surveyDashboard/Table'
+
+function surveyDashboard() {
+    return (
+        < Table />
+    )
+}
+
+export default surveyDashboard
